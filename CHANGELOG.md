420dd832544f996e77f42a6aae9b8ea7b61f5ff3
Zack Pace <pacezipper@gmail.com>
2023-02-22 09:20:35 -0700

    feat: `advance_until` combinator

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,7 @@
 ## 0.3.0-alpha
 - feat!: Introduce `NoMatchError` rather than `()` to be more clear.
 - feat: Implement `ParserIterator` wrapper
+- feat: Implement `advance_until` combinator
 
 ## 0.2.0-alpha
 - feat: Implement `Group` parsers.
--- src/iter/advance.rs
+++ src/iter/advance.rs
@@ -2,11 +2,26 @@ use super::Peek;
 
 /// Advances the provided iterator while the provided condition is true.
 pub fn advance_while<Item>(
-    iter: &mut (impl Iterator<Item = Item> + Peek<Item = Item>),
+    iter: &mut (impl Iterator + Peek<Item = Item>),
     mut cond: impl FnMut(Item) -> bool,
 ) {
     while let Some(item) = iter.peek() {
         if !cond(item) {
+            break;
+        }
+        iter.next();
+    }
+}
+
+/// Advances the provided iterator until the provided condition is true.
+///
+/// Equivalent to `advance_while` with the condition negated.
+pub fn advance_until<Item>(
+    iter: &mut (impl Iterator + Peek<Item = Item>),
+    mut cond: impl FnMut(Item) -> bool,
+) {
+    while let Some(item) = iter.peek() {
+        if cond(item) {
             break;
         }
         iter.next();
