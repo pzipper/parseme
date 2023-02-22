be2e7754fec4fbd0157b843123f7f46605f8edd8
Zack Pace <pacezipper@gmail.com>
2023-02-21 18:45:44 -0700

    fix: iterator infinite loop

--- src/parser/iter.rs
+++ src/parser/iter.rs
@@ -7,12 +7,14 @@ use crate::parser::Parser;
 /// An [Iterator] which wraps a [Parser] and its input.
 ///
 /// Calling [Iterator::next] will call the [Parser::parse] method of the wrapped [Parser], with the
-/// input that the [ParserIter] was initialized with.
+/// input that the [ParserIter] was initialized with.  Once an error has been thrown, the
+/// [ParserIter] will return continuously [None].
 #[derive(Debug, PartialEq, Eq, Hash, PartialOrd, Ord)]
 pub struct ParserIter<'input, 'parser, In: ?Sized, Out, Err, P: Parser<In, Out, Err>> {
     marker: PhantomData<(Out, Err)>,
     input: &'input mut In,
     parser: &'parser mut P,
+    error: bool,
 }
 
 impl<'input, 'parser, In: ?Sized, Out, Err, P: Parser<In, Out, Err>>
@@ -24,6 +26,7 @@ impl<'input, 'parser, In: ?Sized, Out, Err, P: Parser<In, Out, Err>>
             marker: PhantomData,
             input,
             parser,
+            error: false,
         }
     }
 }
@@ -35,6 +38,18 @@ impl<'input, 'parser, In: ?Sized, Out, Err, P: Parser<In, Out, Err>> Iterator
 
     #[inline]
     fn next(&mut self) -> Option<Self::Item> {
-        Some(self.parser.parse(self.input))
+        if self.error {
+            return None;
+        }
+
+        let res = self.parser.parse(self.input);
+
+        match res {
+            Ok(value) => Some(Ok(value)),
+            Err(err) => {
+                self.error = true;
+                Some(Err(err))
+            }
+        }
     }
 }
